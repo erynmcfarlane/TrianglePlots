7565ff0ffd10cd8793a677ff7d563b327132090a
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2023-11-13 17:29:05 -0500

    very few small changes to get heterozygosity (?)

--- plotting_triangles.R
+++ plotting_triangles.R
@@ -21,13 +21,17 @@ for(i in 1:length(datafiles)){
   alldata[[i]]<-fread(datafiles[i], sep=",", header=T)
   alldata[[i]]$m<-as.numeric(rep(m[i], nrow(alldata[[i]]))) # just giving all individuals in the sim the same m and c
   alldata[[i]]$c<-as.numeric(rep(c[i], nrow(alldata[[i]])))
-  alldata[[i]]$mech<-as.factor(rep(mech[i], nrow(alldata[[i]])))
+  #alldata[[i]]$mech<-as.factor(rep(mech[i], nrow(alldata[[i]])))
 }
 
 
 alldata_df<-do.call(rbind.data.frame, alldata)
 
 rm(alldata)
+
+genotypes<-ifelse(alldata_df[,9:518] == 1, 1, 0) 
+### it's still not clear to me how this is different from Q12?
+alldata_df$average_het<-rowMeans(genotypes)
 
 alldata_df[which(alldata_df$gen==10 & alldata_df$m==0.01),]->gen_10_0.01
 alldata_df[which(alldata_df$gen==10 & alldata_df$m==0.2),]->gen_10_0.2
@@ -45,13 +49,13 @@ triangle_cols <- met.brewer("OKeeffe1", 20)
 #for (i in 1:20)
 #{
   #rep_sub <- subset(gen10_deme6, gen10_deme6[,1]==i)
-pdf(file="triangles_for_triangles.pdf", width=10, height=10)
+pdf(file="triangles_for_triangles_10_0.01.pdf", width=10, height=10)
   plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
   segments(0, 0, 0.5, 1, lwd=3)
   segments(1, 0, 0.5, 1, lwd=3)
   for (i in 1:500)
   {
-    points(gen_10[i,5], gen_10[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
+    points(gen_10_0.01[i,5], gen_10_0.01[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
   }
   box(lwd=2)
 dev.off()  
