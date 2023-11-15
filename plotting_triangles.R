1600d8bc5c1417c16e0c8cd6b6fba79dab6b3a55
ErynMcFarlane <eryn.mcfarlane@gmail.com>
2023-11-15 12:15:11 -0500

    saving more generations of the sims to be able to look at gens 1-8 as in Fitzpatrick

--- plotting_triangles.R
+++ plotting_triangles.R
@@ -5,7 +5,7 @@ library(data.table)
 library(MetBrewer)
 library(introgress)
 
-datafiles<-list.files("/gscratch/emcfarl2/TrianglePlots/",  pattern="*u*.main", recursive=TRUE, include.dirs=TRUE)
+datafiles<-list.files("/gscratch/emcfarl2/TrianglePlots/",  pattern="*.main", recursive=TRUE, include.dirs=TRUE)
 ###this would work if I didn't have some other random stuff in there
 basenames<-basename(datafiles)
 m<-str_extract(basenames, "(\\d+\\.*\\d*)")
@@ -33,6 +33,8 @@ genotypes<-ifelse(alldata_df[,9:518] == 1, 1, 0)
 ### it's still not clear to me how this is different from Q12?
 alldata_df$average_het<-rowMeans(genotypes)
 
+alldata_df[which(alldata_df$gen==1 & alldata_df$m==0.01),]->gen_1_0.01
+alldata_df[which(alldata_df$gen==1 & alldata_df$m==0.2),]->gen_1_0.2
 alldata_df[which(alldata_df$gen==10 & alldata_df$m==0.01),]->gen_10_0.01
 alldata_df[which(alldata_df$gen==10 & alldata_df$m==0.2),]->gen_10_0.2
 alldata_df[which(alldata_df$gen==100 & alldata_df$m==0.01 ),]->gen_100_0.01
@@ -49,26 +51,70 @@ triangle_cols <- met.brewer("OKeeffe1", 20)
 #for (i in 1:20)
 #{
   #rep_sub <- subset(gen10_deme6, gen10_deme6[,1]==i)
-pdf(file="triangles_for_triangles_10_0.01.pdf", width=10, height=10)
+pdf(file="triangles_for_triangles_1_0.01.pdf", width=10, height=10)
   plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
   segments(0, 0, 0.5, 1, lwd=3)
   segments(1, 0, 0.5, 1, lwd=3)
   for (i in 1:500)
   {
-    points(gen_10_0.01[i,5], gen_10_0.01[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
+    points(gen_1_0.01[i,5], gen_1_0.01[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
   }
   box(lwd=2)
 dev.off()  
-  
-pdf(file="triangles_for_triangles100.pdf", width=10, height=10)
+
+pdf(file="triangles_for_triangles_10_0.01.pdf", width=10, height=10)
 plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
 segments(0, 0, 0.5, 1, lwd=3)
 segments(1, 0, 0.5, 1, lwd=3)
 for (i in 1:500)
 {
-  points(gen_100[i,5], gen_100[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
+  points(gen_10_0.01[i,5], gen_10_0.01[i,6], pch=21, bg=adjustcolor(triangle_cols[2], alpha.f=0.75), cex=2)
+}
+box(lwd=2)
+dev.off() 
+
+pdf(file="triangles_for_triangles_100_0.01.pdf", width=10, height=10)
+plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
+segments(0, 0, 0.5, 1, lwd=3)
+segments(1, 0, 0.5, 1, lwd=3)
+for (i in 1:500)
+{
+  points(gen_100_0.01[i,5], gen_100_0.01[i,6], pch=21, bg=adjustcolor(triangle_cols[3], alpha.f=0.75), cex=2)
+}
+box(lwd=2)
+dev.off() 
+
+
+pdf(file="triangles_for_triangles_1_0.2.pdf", width=10, height=10)
+plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
+segments(0, 0, 0.5, 1, lwd=3)
+segments(1, 0, 0.5, 1, lwd=3)
+for (i in 1:500)
+{
+  points(gen_1_0.2[i,5], gen_1_0.2[i,6], pch=21, bg=adjustcolor(triangle_cols[1], alpha.f=0.75), cex=2)
 }
 box(lwd=2)
 dev.off()  
 
+pdf(file="triangles_for_triangles_10_0.2.pdf", width=10, height=10)
+plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
+segments(0, 0, 0.5, 1, lwd=3)
+segments(1, 0, 0.5, 1, lwd=3)
+for (i in 1:500)
+{
+  points(gen_10_0.2[i,5], gen_10_0.2[i,6], pch=21, bg=adjustcolor(triangle_cols[2], alpha.f=0.75), cex=2)
+}
+box(lwd=2)
+dev.off() 
+
+pdf(file="triangles_for_triangles_100_0.2.pdf", width=10, height=10)
+plot(0, type="n", xlab="q", ylab="Q", xlim=c(0, 1), ylim=c(0, 1), las=1, cex.lab=1.5, cex.axis=1.25)
+segments(0, 0, 0.5, 1, lwd=3)
+segments(1, 0, 0.5, 1, lwd=3)
+for (i in 1:500)
+{
+  points(gen_100_0.2[i,5], gen_100_0.2[i,6], pch=21, bg=adjustcolor(triangle_cols[3], alpha.f=0.75), cex=2)
+}
+box(lwd=2)
+dev.off() 
 
